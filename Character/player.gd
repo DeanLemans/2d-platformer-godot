318839c7cb4599a7da26ec24b54364ecd20b1460
Dean <111811264+DeanLemans@users.noreply.github.com>
2023-10-08 22:36:01 +0200

    var speed n jump-velocity

--- Character/player.gd
+++ Character/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
+# template did not make myself.
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed : float = 300.0
+@export var JUMP_VELOCITY : float = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -21,8 +21,8 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
