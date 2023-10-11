d835b9d2d9780d021170861af3074c5f0c69bf54
Dean <111811264+DeanLemans@users.noreply.github.com>
2023-10-11 09:29:04 +0200

    animation template added

--- Character/player.gd
+++ Character/player.gd
@@ -2,20 +2,29 @@ extends CharacterBody2D
 # template did not make myself.
 
 @export var speed : float = 250.0
-@export var jump_velocity : float = -200.0
+@export var jump_velocity : float = -150.0
+@export var double_jump_velocity : float = -150
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var has_double_jumped : bool = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		has_double_jumped = false
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			#normal jump from floor
+			velocity.y = jump_velocity
+		elif not has_double_jumped:
+			#double jump in air
+			velocity.y += double_jump_velocity
+			has_double_jumped = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
