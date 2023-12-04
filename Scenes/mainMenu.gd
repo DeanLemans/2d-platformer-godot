50e00e7854588a87b387062ea0c7345061f9518a
Dean <111811264+DeanLemans@users.noreply.github.com>
2023-12-04 16:55:20 +0100

    ye

--- Scenes/mainMenu.gd
+++ Scenes/mainMenu.gd
@@ -4,5 +4,8 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/test_level.tscn")
 #C:\Users\Dean\2d-platformer-godot\Levels
 
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/test_level.tscn")
+
 func _on_quit_pressed():
 	get_tree().quit()
