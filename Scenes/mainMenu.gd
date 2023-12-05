2d4dbf3b0cbceb4ddacd6b31167dcf0790f8b5a6
Dean <111811264+DeanLemans@users.noreply.github.com>
2023-12-05 12:25:06 +0100

    file system improvement

--- Scenes/mainMenu.gd
+++ Scenes/mainMenu.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/test_level.tscn")
-#C:\Users\Dean\2d-platformer-godot\Levels
+func _on_playAmbitie_pressed():
+	get_tree().change_scene_to_file("res://Scenes/ambitieLevel.tscn")
+#res://Scenes/ambitieLevel.tscn
 
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/test_level.tscn")
+func _on_playGlow_pressed():
+	get_tree().change_scene_to_file("res://Scenes/glowLevel.tscn")
+#res://Scenes/glowLevel.tscn
 
 func _on_quit_pressed():
 	get_tree().quit()
