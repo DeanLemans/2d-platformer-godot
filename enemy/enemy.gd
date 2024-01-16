56414d2ea93ed04e63bed7f56cf440798eb19a8d
Dean <111811264+DeanLemans@users.noreply.github.com>
2024-01-16 13:30:55 +0100

    level selection

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
+var direction : Vector2 = Vector2.ZERO
+var animation_player : AnimationPlayer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(delta: float) -> void:
+	# Your existing direction checking logic
+	if direction.x > 0:
+		animation_player.scale.x = 1
+	elif direction.x < 0:
+		animation_player.scale.x = -1
