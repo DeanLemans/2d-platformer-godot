02c89b8fb44efa641e22d00a954190c6e1bcfe8a
Dean <111811264+DeanLemans@users.noreply.github.com>
2024-01-23 13:41:48 +0100

    fixed enemy kills player

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -4,12 +4,9 @@ var direction : Vector2 = Vector2.ZERO
 var animation_player : AnimationPlayer
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta: float):
 	# Your existing direction checking logic
 	if direction.x > 0:
 		animation_player.scale.x = 1
@@ -17,6 +14,6 @@ func _process(delta: float) -> void:
 		animation_player.scale.x = -1
 
 
-func _on_area_2d_area_entered(area):
-	get_tree().change_scene_to_file("res://Scenes/lost.tscn")
-	#
+func _on_area_2d_body_entered(body: Node2D):
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_file("res://Scenes/lost.tscn")
