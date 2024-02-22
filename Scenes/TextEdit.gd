270f73ed5325623e68a757ed8d202442615b8f90
Dean <111811264+DeanLemans@users.noreply.github.com>
2024-02-22 14:42:13 +0100

    ESC on lost scene brings too level selection
    instead too main menu

--- Scenes/TextEdit.gd
+++ Scenes/TextEdit.gd
@@ -6,4 +6,4 @@ func _ready():
 
 func _process(_delta):
 	if Input.is_action_pressed("ui_cancel"):
-		get_tree().change_scene_to_file("res://Scenes/mainMenu.tscn")
+		get_tree().change_scene_to_file("res://Scenes/level_selection.tscn")
